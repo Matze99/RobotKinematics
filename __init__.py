887315ed18fdf524f643bfc7fdeadf4e7c39268e
Matthias Hamacher <hamachermatthias12@gmail.com>
2021-02-22 17:40:06 +0100

    removed init

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from math_utils import Vector, Matrix
-from robot import Robot, JointType, Joint, BaseJoint, RotationalJoint, PrismaticJoint, EndEffector
+# from math_utils import Vector, Matrix
+# from robot import Robot, JointType, Joint, BaseJoint, RotationalJoint, PrismaticJoint, EndEffector
